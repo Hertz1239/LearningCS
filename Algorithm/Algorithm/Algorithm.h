7c6d27e756e31cbf5883993982d37311d7ada2d9
Hertz <hertz1239@gmail.com>
2016-05-27 16:25:19 +0800

    setup Algorithm Project and its XCTest

---
+++ Algorithm/Algorithm/Algorithm.h
@@ -0,0 +1,19 @@
+//
+//  Algorithm.h
+//  Algorithm
+//
+//  Created by Hertz on 5/27/16.
+//  Copyright © 2016 Hertz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Algorithm.
+FOUNDATION_EXPORT double AlgorithmVersionNumber;
+
+//! Project version string for Algorithm.
+FOUNDATION_EXPORT const unsigned char AlgorithmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algorithm/PublicHeader.h>
+
+
---
+++ Algorithm/AlgorithmTests/AlgorithmTests.m
@@ -0,0 +1,39 @@
+//
+//  AlgorithmTests.m
+//  AlgorithmTests
+//
+//  Created by Hertz on 5/27/16.
+//  Copyright © 2016 Hertz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlgorithmTests : XCTestCase
+
+@end
+
+@implementation AlgorithmTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
